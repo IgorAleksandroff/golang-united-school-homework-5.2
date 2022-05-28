df3c2ee4bbc38f617e172b61f9c5e97c54fdc0cb
Igor Aleksandrov <ivaleksandrov@avito.ru>
2022-05-28 10:02:17 +0300

    first solution

--- cache.go
+++ cache.go
@@ -2,22 +2,47 @@ package cache
 
 import "time"
 
+type valueWithTime struct {
+	value        string
+	notCanExpire bool
+	deadline     time.Time
+}
+
 type Cache struct {
+	memo map[string]valueWithTime
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		make(map[string]valueWithTime),
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c *Cache) Get(key string) (string, bool) {
+	valWithTime, ok := c.memo[key]
+	return valWithTime.value, ok && (valWithTime.notCanExpire || !time.Now().After(valWithTime.deadline))
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.memo[key] = valueWithTime{
+		value:        value,
+		notCanExpire: true,
+	}
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	keys := make([]string, 0, len(c.memo))
+	for key := range c.memo {
+		if _, ok := c.Get(key); ok {
+			keys = append(keys, key)
+		}
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.memo[key] = valueWithTime{
+		value:    value,
+		deadline: deadline,
+	}
 }
